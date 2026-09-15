59d05c3f4eedca915c91cca15d1790aea95b81a5
Lauri Palonen <38961934+lauripalonen@users.noreply.github.com>
2026-09-15 17:09:18 +0300

    Fetch watermelon nutrition data for smoothies
    Reintroduced API call to fetch watermelon nutrition data for selected ingredients in the smoothie app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import os
 from snowflake.snowpark.functions import col
 import requests  
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
@@ -36,7 +34,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
